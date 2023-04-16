72d101b2fdf4b5d3f496cdbb583eef9bc4546ade
Monico Torres <asaelgarcia45@gmail.com>
2023-04-16 00:08:03 -0600

    vista creada

---
+++ app/src/main/java/com/example/brainboost/Courses/views/Comments.java
@@ -0,0 +1,16 @@
+package com.example.brainboost.Courses.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.brainboost.R;
+
+public class Comments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comments);
+    }
+}
